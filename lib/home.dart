327b234c40c648a873f5dd2e1621a8d485e94ebc
Mario2309 <151546364+Mario2309@users.noreply.github.com>
2025-04-22 15:19:14 +0200

    update

--- lib/home.dart
+++ lib/home.dart
@@ -13,9 +13,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MainPage(),
-    );
+    return MaterialApp(home: MainPage());
   }
 }
 
@@ -26,11 +24,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
-  final List<Widget> _pages = [
-    HomePage(),
-    SearchPage(),
-    ProfilePage(),
-  ];
+  final List<Widget> _pages = [HomePage(), SearchPage(), ProfilePage()];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -41,56 +35,44 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bottom Navigation Bar Example'),
-      ),
-      body: Center(
-        child: _pages.elementAt(_selectedIndex),
-      ),
+      appBar: AppBar(title: const Text('Bottom Navigation Bar Example')),
+      body: Center(child: _pages.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
         onTap: _onItemTapped,
       ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddProductScreen()),
-                );
-              },
-              child: const Icon(Icons.add_box_outlined),
-              heroTag: "addProduct", // Unique tag for the first button
-            ),
-            SizedBox(width: 16),
-            FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddEmployeeScreen()),
-                );
-              },
-              child: const Icon(Icons.person_add_alt_1),
-              heroTag: "addEmployee", // Unique tag for the second button
-            ),
-          ]),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddProductScreen()),
+              );
+            },
+            child: const Icon(Icons.add_box_outlined),
+            heroTag: "addProduct", // Unique tag for the first button
+          ),
+          SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddEmployeeScreen()),
+              );
+            },
+            child: const Icon(Icons.person_add_alt_1),
+            heroTag: "addEmployee", // Unique tag for the second button
+          ),
+        ],
+      ),
     );
   }
 }
@@ -114,53 +96,54 @@ class _HomePageState extends State<HomePage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text("Products",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+            child: Text(
+              "Products",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
           products.isEmpty
               ? Center(child: Text("No products data"))
               : SizedBox(
                 height: 200, // Set a fixed height for the list
                 child: ListView.builder(
-                    itemCount: products.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(products[index].name),
-                      );
-                    },
-                  ),
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(title: Text(products[index].name));
+                  },
+                ),
               ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Employees",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Employees",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          employees.isEmpty
+              ? Center(child: Text("No employees data"))
+              : SizedBox(
+                height: 200, // Set a fixed height for the list
+                child: ListView.builder(
+                  itemCount: employees.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(employees[index].name),
+                      subtitle: Text(employees[index].position),
+                    );
+                  },
+                ),
               ),
-              employees.isEmpty
-                  ? Center(child: Text("No employees data"))
-                  : SizedBox(
-                    height: 200, // Set a fixed height for the list
-                    child: ListView.builder(
-                        itemCount: employees.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(employees[index].name),
-                            subtitle: Text(employees[index].position),
-                          );
-                        },
-                      ),
-                  ),
         ],
       ),
     );
-    
   }
 }
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Search Page'),
-    );
+    return const Center(child: Text('Search Page'));
   }
 }
 
@@ -170,14 +153,19 @@ class UserData {
   final String description;
   final String image;
 
-  UserData({this.name = 'User Name', this.email = 'user@email.com', this.description = 'User description', this.image = 'assets/default_profile.jpg'});
+  UserData({
+    this.name = 'User Name',
+    this.email = 'user@email.com',
+    this.description = 'User description',
+    this.image = 'assets/default_profile.png',
+  });
 }
 
 class ProfilePage extends StatelessWidget {
   final UserData userData = UserData();
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -185,7 +173,7 @@ class ProfilePage extends StatelessWidget {
           CircleAvatar(
             radius: 60,
             backgroundImage: AssetImage(userData.image),
-            backgroundColor: Colors.grey[200], 
+            backgroundColor: Colors.grey[200],
           ),
           const SizedBox(height: 20),
           Text(
